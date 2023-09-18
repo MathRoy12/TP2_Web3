1f2e45e15b06b32bef5a5a45bd69e9396b978ac3
Mathieu Roy <2234476@cegepmontpetit.ca>
2023-09-18 09:32:07 -0400

    Ajout Path

--- TP2/src/app/app.module.ts
+++ TP2/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from "@angular/router";
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule }   from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { AccueilComponent } from './accueil/accueil.component';
-import { ArtisteComponent } from './artiste/artiste.component';
-import { AlbumComponent } from './album/album.component';
-import { ConcertComponent } from './concert/concert.component';
+import {AppComponent} from './app.component';
+import {AccueilComponent} from './accueil/accueil.component';
+import {ArtisteComponent} from './artiste/artiste.component';
+import {AlbumComponent} from './album/album.component';
+import {ConcertComponent} from './concert/concert.component';
 
 @NgModule({
   declarations: [
@@ -22,9 +22,17 @@ import { ConcertComponent } from './concert/concert.component';
     HttpClientModule,
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/accueil', pathMatch: 'full'},
+      {path: 'accueil', component: AccueilComponent},
+      {path: 'artiste/:name', component: ArtisteComponent},
+      {path: ':albumName/album', component: AlbumComponent},
+      {path: ':artistName/concert', component: ConcertComponent},
+      {path:'**', component:AccueilComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
